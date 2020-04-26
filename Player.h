30d94fff8942a18f009d27410fe5b618f458be79
Raul Lima Alves <raullalves@gmail.com>
2020-04-26 15:23:37 -0300

    adding support to windows and ffmpeg 4

---
+++ Player.h
@@ -0,0 +1,136 @@
+#include "stdafx.h"
+
+typedef struct _AudioPacket
+	{
+		AVPacketList *first, *last;
+		int nb_packets, size;
+  		SDL_mutex *mutex;
+  		SDL_cond *cond;
+	} AudioPacket;
+
+class Player {
+
+public:
+	
+	//construtor
+	Player(std::string endereco) {
+
+		audioStream = -1;
+
+		//open video
+		int res = avformat_open_input(&pFormatCtx, endereco.c_str(), NULL, NULL);
+
+		//check video opened
+		if (res!=0){
+			exibirErro(res);
+			exit(-1);
+		}
+
+		//get video info
+		res = avformat_find_stream_info(pFormatCtx, NULL);
+		if (res < 0) {
+			exibirErro(res);
+			exit(-1);
+		}
+
+		//get video stream
+		videoStream = obterCodecParameters();
+		if (videoStream == -1) {
+			std::cout << "Error opening your video using AVCodecParameters, does not have codecpar_type type AVMEDIA_TYPE_VIDEO" << std::endl;
+			exit(-1);
+		}
+
+		if (lerCodecVideo() < 0) exit(-1);
+
+	}
+
+	~Player(void) {
+
+		av_free(buffer);
+		av_free(pFrameRGB);
+
+		// Free the YUV frame
+		av_free(pFrame);
+
+		// Close the codecs
+		avcodec_close(pCodecCtx);
+
+		// Close the video file
+		avformat_close_input(&pFormatCtx);
+
+	}
+
+	
+	int alocarMemoria(void);
+	int lerFramesVideo(void);
+	int criarDisplay(void);
+	
+	static int getAudioPacket(AudioPacket*, AVPacket*, int);
+
+private:
+	
+	void memsetAudioPacket(AudioPacket * pq);
+	//armazena o �ndice do determinado Stream a ser transmitido
+	int videoStream;
+
+	//stream de audio
+	int audioStream;
+
+	//contem informa��es sobre o arquivo de v�deo, incluindo os codecs, etc
+	AVFormatContext *pFormatCtx = NULL;
+
+	//contem informa��es do codec do v�deo, obtidas atraves de
+	//pFormatCtx->streams[i]->codecpar
+	//olhando o codec_type e vendo se � transmissao de video do tipo AVMEDIA_TYPE_VIDEO
+	AVCodecParameters *pCodecParameters = NULL;
+
+	//Audio COdec Parametrs
+	AVCodecParameters *pCodecAudioParameters = NULL;
+
+	//informa��es do codecParameters, por�m copiadas. o pCodecParameters serve como um backup das informa��es do v�deo
+	AVCodecContext *pCodecCtx = NULL;
+
+	AVCodecContext *pCodecAudioCtx = NULL;
+
+	SDL_AudioSpec wantedSpec = { 0 }, audioSpec = { 0 };
+
+	//guarda o codec do v�deo
+	AVCodec *pCodec = NULL;
+
+	//guarda o codec do audio
+	AVCodec *pAudioCodec = NULL;
+
+	//estrutura que guarda o frame
+	AVFrame *pFrame = NULL;
+
+	//estrutura que guarda o frame RGB
+	AVFrame *pFrameRGB = NULL;
+
+	//buffer para leitura dos frames
+	uint8_t *buffer = NULL;
+
+	//estrutura que armazena a convers�o para RGB
+	struct SwsContext *sws_ctx = NULL;
+
+	//surface window para exibir o video
+	//pode ter m�ltiplas screens
+	SDL_Window *screen;
+
+	SDL_Renderer *renderer;
+
+	SDL_Texture* bmp;
+	
+	//exibe o erro com rela��o ao seu respectivo c�digo
+	void exibirErro(int erro);
+
+	int obterCodecParameters(void);
+
+	int lerCodecVideo(void);
+
+	int PacketQueuePut(AudioPacket *, const AVPacket *);
+
+	void initAudioPacket(AudioPacket *); 
+
+	int putAudioPacket(AudioPacket *, AVPacket *); 
+
+};
--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# Simple C++ video player using FFmpeg 3 and SDL 2.0 
+# Simple C++ video player using FFmpeg 4 and SDL 2.0 
 
 Supports video with audio.
-## Windows setup
+## Windows setup (Tested on x64 environment)
 1 - Install FFMpeg here https://ffmpeg.zeranoe.com/builds/win64/dev/ and add it to your path
 2 - Install SDL here https://www.libsdl.org/download-2.0.php and add it to your path
 3 - (Optional) Install g++ and make from MinGW https://sourceforge.net/projects/mingw/
+4 - Create a env variable to the /include folder of the FFmpeg folder: <FFmpeg_include>, and for the lib folder <FFmpeg_lib>
+5 - Create a env variable to the /include folder of the SDL2 folder: <SDL2_include>
 
-1 - Install FFmpeg version >= 3.2.4
+6 - Run make
+        <br />
+        ```
+        make -f makefile_windows
+        ```
+
+7 - Run<br />
+    ```
+    ./player <video_address>
+    ```
+
+## Linux setup (Tested on Ubuntu)
+
+1 - Install FFmpeg version >= 4
         <br />
         ```
         sudo apt update && sudo apt install ffmpeg x264 x265 libavdevice-dev
